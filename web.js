60c1998fa1996d2c4356cfd2237bc12288a5d5f6
Taimur Khaliq <khaliq.taimur@gmail.com>
2014-01-05 22:02:40 -0500

    Using express and basic html to test rest apis

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(process.env.PORT || 5000);
+var express = require('express');
+var app = express();
+app.configure(function () {
+    app.use(
+        "/", //the URL throught which you want to access to you static content
+        express.static(__dirname) //where your static content is located in your filesystem
+    );
+});
+app.listen(process.env.PORT || 5000); //the port you want to use
+app.get("/makeBox", function(req,res) {
+    res.send("MAKING U BOX BITCH");
+});
